a48730ff5eb56e4ca25e92f27a2fc56a7057dc95
JAY-Winter <rubat0@naver.com>
2024-01-24 23:33:33 +0900

    AppConfig 를 통한 새로운 할인 정책 적용

--- src/main/java/hello/core/order/AppConfig.java
+++ src/main/java/hello/core/order/AppConfig.java
@@ -2,6 +2,7 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -21,7 +22,8 @@ public class AppConfig {
     }
 
     private static DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy(); // AppConfig 만 변경해 실제 DiscountPolicy 구현체를 주입한다
     }
 
 }
